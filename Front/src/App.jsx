bcefe978f400fefd991e3073fc071d80437629d5
AnUnknownC <danesc401@hotmail.com>
2026-08-11 19:53:42 -0500

    UI only based in Tailwind UI

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import roscon from "./assets/roscon.jpeg";
+import { useEffect, useState } from 'react'
+import heroVid from './assets/vecteezy_eating-dinner-in-restaurant_22933277.mov'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-    <header>
-      <nav className='superior'>
-        <div className='nombre'>Daniel Sanchez</div>
-        <ul className='apartados'>
-          <li className='parte'>Inicio</li>
-          <li className='parte'>Proyectos</li>
-          <li className='parte'>Contacto</li>
-        </ul>
-      </nav>
+    <header className='fixed top-0 left-0 z-50 flex w-full h-[100px] justify-between border-b border-black items-center bg-transparent'>
+      <div className='!ml-12'>Sazón de la Abuela</div>
+      <ul className='flex flex-row gap-10'>
+        <li className=''>Inicio</li>
+        <li className=''>Proyectos</li>
+        <li className=''>Contacto</li>
+      </ul>
+      <button className='h-[40px] w-[150px] !mr-12 border border-black'>Reservar mesa</button>
     </header>
-    <div class="website">
-      <section className='sidebar'>
+    <div className="">
+      <section className="relative w-full h-[600px] overflow-hidden shadow-lg !mb-12">
+        <video src={heroVid} autoPlay loop className='w-full h-full object-cover'></video>
+        <div className='absolute top-[100px] left-[50px] h-[400px] w-[600px] *:text-left *:text-white'>
+          <p className='font-serif !ml-10 !mt-4 h-[30px] w-[500px]'>Sazón de la Abuela</p>
+          <h1 className='!playfair !ml-10 !my-5 h-[150px] w-[500px] text-xl'>Como en casa. Mejor que en casa.</h1>
+          <p className='font-serif !ml-10 h-[100px] w-[500px]'>Sazón de la Abuela lleva más de 25 años cocinando lo de siempre con lo de aquí. Caldo de gallina, cocido de los jueves, tarta de manzana que sale del horno a las doce. Sin cartas enormes. Sin artificios.</p>
+        </div>
       </section>
-      <section class="content">
-          <div class="plato"><img src={roscon} alt="roscon"/><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-          <div class="plato"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-          <div class="plato"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-          <div class="plato"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-          <div class="plato"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
-          <div class="plato"><p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p></div>
+      <section className='flex flex-wrap gap-6 justify-center'>
+        <div className='w-[350px] h-[350px] bg-red-600'>
+          <img src="" alt="" className='w-full h-[70%] bg-yellow-600'/>
+          <p className='w-full h-[30%] bg-yellow-200'></p>
+        </div>
+        <div className='w-[350px] h-[350px] bg-red-600'>
+          <img src="" alt="" className='w-full h-[70%] bg-yellow-600'/>
+          <p className='w-full h-[30%] bg-yellow-200'></p>
+        </div>
+        <div className='w-[350px] h-[350px] bg-red-600'>
+          <img src="" alt="" className='w-full h-[70%] bg-yellow-600'/>
+          <p className='w-full h-[30%] bg-yellow-200'></p>
+        </div>
+        <div className='w-[350px] h-[350px] bg-red-600'>
+          <img src="" alt="" className='w-full h-[70%] bg-yellow-600'/>
+          <p className='w-full h-[30%] bg-yellow-200'></p>
+        </div>
       </section>
     </div>
     </>
